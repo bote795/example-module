0a450c85f7941f409ff9dc46d276f348a9de882a
Nicolas Botello <nbotell2@gmail.com>
2018-07-03 22:55:57 -0700

    webpack basics

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-console */
+const defaultAwesomeFunction = require("../dist");
+
+const defaultVal = defaultAwesomeFunction("Nick");
+
+// defaultVal === 'I am the Default Awesome Function, fellow comrade! - Nick'
+console.log(defaultVal);
